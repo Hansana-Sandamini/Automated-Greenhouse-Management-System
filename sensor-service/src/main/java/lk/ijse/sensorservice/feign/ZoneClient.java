baf2866f381d1be363eddd7f262041f3c4ec661e
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2026-04-09 23:43:45 +0530

    Implement OpenFeign ZoneClient to fetch zone data from zone-service

---
+++ sensor-service/src/main/java/lk/ijse/sensorservice/feign/ZoneClient.java
@@ -0,0 +1,11 @@
+package lk.ijse.sensorservice.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name = "zone-service")
+public interface ZoneClient {
+
+    @GetMapping("/api/v1/zones/{id}")
+    Object getZoneById(@PathVariable("id") Long id);
+}
